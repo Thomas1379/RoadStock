dc146d41e870bb678111449e39de927a1758a1c7
Thomas DRAGHI <thomas@Mac.lan>
2026-09-08 19:50:48 +0200

    feat: initialisation du projet RoadStock (backend Spring Boot, frontend Angular, docker-compose postgres)

---
+++ backend/src/main/java/fr/roadstock/api/RoadstockApiApplication.java
@@ -0,0 +1,13 @@
+package fr.roadstock.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadstockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadstockApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fr/roadstock/api/RoadstockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.roadstock.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadstockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
